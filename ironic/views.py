e5c50b7d54b707920d1bfc325ce534133535f9eb
disgoy <dishagoyal1505@gmail.com>
2024-10-20 18:21:16 +0530

    feat: response to ironic

--- ironic/views.py
+++ ironic/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+# If it's a post request, get the information and make a http response to an api and then return the contents of it inside HTTPResponse()
 def index(request):
-    return render(request, "ironic/index.html")
+    if (request.method == 'GET'):
+        return render(request, "ironic/index.html")
+    else:
+        
+        print('bro wtf')
+        return HttpResponse("ayo")
