96562fdd55acb6fcd9acc2cc187e7a08a1f4f34b
Disha Goyal <disha@Dishas-MacBook-Air.local>
2024-10-20 16:13:54 +0530

    feat: added models

--- ironic/models.py
+++ ironic/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Texts(models.Model):
+    text_input=models.TextField()
+    classification=models.TextField(null=True)
+    text_response=models.TextField(null=True)
+    created_at= models.DateTimeField()
