993672130ac9f104d63a16aec10db720e8f0ceef
disgoy <dishagoyal1505@gmail.com>
2024-10-21 18:59:06 +0530

    feat: added isIronic function

--- ironic/views.py
+++ ironic/views.py
@@ -9,25 +9,28 @@ load_dotenv()
 
 # Create your views here.
 # If it's a post request, get the information and make a http response to an api and then return the contents of it inside HTTPResponse()
-def getLLMResponse(inp, llm="gpt-4o-mini"):
-        openai_key = os.getenv("OPENAI_KEY")
-        openai_url = 'https://api.openai.com/v1/chat/completions'
-        data = { "model": llm,
-                "messages": [{"role": "user", "content": inp}]
-            }
-        headers = {"Content-Type": "application/json", "Authorization": f"Bearer {openai_key}"}
-        response = requests.post(openai_url, json=data, headers=headers)
-        responsedata = response.json()
-        output = responsedata['choices'][0]['message']['content']
-        print(llm)
-        return output
-
 def index(request):
-    if (request.method == 'GET'):
-        return render(request, "ironic/index.html")
-    else:
-        input = request.POST.get('message')
-        output = getLLMResponse(input, "gpt-3.5-turbo")
-        print(output)
-        return JsonResponse({"data": output, "message": "Response successfully generated", "success" : True})
+    if (request.method != 'POST'):
+       return JsonResponse({
+            "error": "Method not allowed",
+            "message": "Only POST requests are supported for this endpoint",
+            "success": False
+        }, status=405)
+    
+    input_message = request.POST.get('message')
 
+    if not input_message:
+        return JsonResponse({
+            "error": "Missing parameters",
+            "message": "'message' is required",
+            "success": False
+        }, status=400)
+
+    output = isIronic(input_message)
+    print(output)
+    return JsonResponse({"tone": {
+        "type" : "ironic", "value" : output
+    }, "message": "Response successfully generated", "success" : True})
+
+def isIronic(message):
+    return True
