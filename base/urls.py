d4abe8273544e84622d892b57076d6f86db86af6
disgoy <dishagoyal1505@gmail.com>
2024-10-21 18:36:45 +0530

    feat: added /generate route

--- base/urls.py
+++ base/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # ex: /ironic/
-    path("", views.index, name="index"),
+    path("generate", views.index, name='LLMResponse')
 ]
